5af330f3a6806a7629a439c9f98bba988beacfbf
Calm <53460165+tamecalm@users.noreply.github.com>
2024-06-09 19:58:34 +0100

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,1 +1,6 @@
-
+TWITTER_API_KEY = "your_twitter_api_key"
+TWITTER_API_SECRET = "your_twitter_api_secret"
+TWITTER_ACCESS_TOKEN = "your_twitter_access_token"
+TWITTER_ACCESS_TOKEN_SECRET = "your_twitter_access_token_secret"
+TELEGRAM_BOT_TOKEN = "your_telegram_bot_token"
+CHIPPER_CASH_API_KEY = "your_chipper_cash_api_key"
